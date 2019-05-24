eff6a4af4d2aecc76a63d90378fd3f73a985205b
Mateo Martinjak <mateo1559@gmail.com>
2019-05-24 22:11:59 +0200

    resizable window created

--- openGL_project_01/main.cpp
+++ openGL_project_01/main.cpp
@@ -1,20 +1,89 @@
-
+//ORIGIN MASTER
 #include "libs.h"
+
+void framebuffer_resize_callback(GLFWwindow* window, int fbW, int fbH)
+{
+	glViewport(0, 0, fbW, fbH);
+
+}
+
+
 
 int main() 
 {
 
-	//ORIGIN MASTER
+	
+
+	//INIT GLFW
+
 	glfwInit();
 
-	glm::vec3(0.f);
+
+	//CREATE WINDOW
+	const int WINDOW_WIDTH = 640;
+	const int WINDOW_HEIGHT = 480;
+
+			//canvas INSIDE the window
+	int framebufferWidth = 0;
+	int framebufferHeight = 0;
+
+			//using VERSION 4.4
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+
+	GLFWwindow* window = glfwCreateWindow(
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		"FIRST OPENGL WINDOW",
+		NULL,NULL);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+
+
+	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+				//canvas size
+	//glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+	glfwMakeContextCurrent(window); //important for init.
+
+	//INIT GLEW(NEEDS WINDOW AND OPENGL CONTEXT)
 
 	glewExperimental = GL_TRUE;
 
-	if (glewInit() != GLEW_OK)
-		std::cout << "FATAL ERROR";
+	if (glewInit() != GLEW_OK) {
+		std::cout << "ERROR::MAIN.CPP::GLEW_INIT_FAILED" << "\n";
+		glfwTerminate();
+	}
 
-	system("PAUSE");
+	//MAIN LOOP
+	while(!glfwWindowShouldClose(window))
+	{
+		//update input
+		glfwPollEvents();
 
+		//update
+
+		//draw
+
+		//clear
+		glClearColor(0.f, 1.f, 1.f, 1.f);
+		glClear(
+			GL_COLOR_BUFFER_BIT | 
+			GL_DEPTH_BUFFER_BIT | 
+			GL_STENCIL_BUFFER_BIT);
+
+
+		//end
+		glfwSwapBuffers(window);
+		glFlush();
+	}
+
+
+	//PROGRAM END
+	
+	glfwTerminate();
 	return 0;
 }
